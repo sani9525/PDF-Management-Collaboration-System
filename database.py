9ace7bfe28e4bc460da149661918af87b7a2ed2c
sani <sanikumar9525462150@gmail.com>
2025-05-26 22:02:26 +0530

    edit data

--- database.py
+++ database.py
@@ -1,9 +1,20 @@
+import os
 import asyncpg
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+load_dotenv()
 
 async def get_connection():
+    url = os.getenv("DATABASE_URL")
+    if not url:
+        raise ValueError("DATABASE_URL not set")
+
+    parsed = urlparse(url)
+
     return await asyncpg.connect(
-        host="localhost",
-        database="pdf_system",
-        user="postgres",
-        password="Sani@123",
-)
+        user=parsed.username,
+        password=parsed.password,
+        database=parsed.path.lstrip("/"),
+        host=parsed.hostname,
+        port=parsed.port or 5432
+    )
